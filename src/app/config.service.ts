88bcce031d5fd73a23e18df28772d093f594f1ec
rgirodon <remy.girodon@gmail.com>
2021-12-03 12:00:02 +0100

    Set BehaviourSubject in service

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Config } from './config';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,21 @@ export class ConfigService {
 
   configUrl = 'assets/config.json';
 
+  config = new BehaviorSubject<Config>({
+    api_url: '',
+    text_file: '',
+    date: ''
+  });
+
   constructor(private http: HttpClient) { }
 
-  getConfig() {
-    return this.http.get<Config>(this.configUrl);
+  loadConfig() {
+    
+    this.http.get<Config>(this.configUrl).subscribe(
+      
+     data => {
+       this.config.next(data);
+     }
+    );
   }
 }
