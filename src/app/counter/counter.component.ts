e1f97212767edf8e087aac104506806839a8b55b
rgirodon <remy.girodon@gmail.com>
2024-08-26 10:56:55 +0200

    Add basic counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  value: number = 0;
+
+  increment(): void {
+
+    this.value++;
+  }
+}
