e21f646d6ba3eca70b852968b07e04f1e0d62637
rgirodon <remy.girodon@gmail.com>
2024-08-26 16:08:29 +0200

    And input fields

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { LoggerService } from '../logger.service';
 
 @Component({
@@ -8,16 +8,25 @@ import { LoggerService } from '../logger.service';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
 
-  value: number = 0;
+  value: number = 0; 
+
+  @Input() initialValue: number = 0; 
+
+  @Input() incrementValue: number = 1;
 
   increment(): void {
 
     this.logger.log("Incrementing value...");
 
-    this.value++;
+    this.value = this.value + this.incrementValue;
   }
 
-  constructor(private logger: LoggerService) { }
+  constructor(private logger: LoggerService) { 
+  }
+
+  ngOnInit(): void {
+    this.value = this.initialValue;
+  }
 }
