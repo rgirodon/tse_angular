64867fe01876cff54a497809a27e99011fec91a5
rgirodon <remy.girodon@gmail.com>
2024-08-27 16:30:12 +0200

    Remove async pipe and use real subscriber

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Observable } from 'rxjs';
 import { Hero } from '../hero';
-import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent implements OnInit {
 
-  hero$!: Observable<Hero>;
+  hero!: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-    this.hero$ = this.heroService.getHero();
+    this.heroService.getHero().subscribe((hero: Hero) => {
+
+      this.hero = hero;
+    });
   }
 }
